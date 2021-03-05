27622c618f2f81707847baca5517856383ba833b
Rodrigo Gutierrez <rgutierrez@adit-jv.com>
2021-03-06 02:24:18 +0900

    generate meta data from files

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
+
+from os import listdir
+from os.path import dirname, isfile, join, splitext
 
 import logging
 import simplejson as json
 
-robots = [
-    {"name": "Bahamut", "id": 1},
-    {"name": "Shiva", "id": 2},
-    {"name": "Ifrit", "id": 3}]
+robots = []
+
+def generateRobots():
+    relPath = dirname(__file__)
+    mypath = join(relPath, "instance")
+    for f in listdir(mypath):
+        if isfile(join(mypath, f)):
+            res = json.loads(open(join(mypath, f), "r").read())
+            res["id"] = int(splitext(f)[0])
+            robots.append(res)
 
 app = Flask(__name__)
 
@@ -17,14 +26,14 @@ def hello():
 @app.route("/robots", methods=("GET", "POST"))
 def robotsBase():
     if request.method == "GET":
-        return json.dumps(robots)
+        return jsonify(robots)
 
     if request.method == "POST":
         name = request.args.get("name")
         size = len(robots)
         robot = {"name": name, "id": size + 1}
         robots.append(robot)
-        return json.dumps(robot)
+        return robot
 
 @app.route("/robots/<int:id>", methods=("GET", "PUT", "DELETE"))
 def robotsWithID(id):
@@ -32,8 +41,9 @@ def robotsWithID(id):
         for robot in robots:
             if robot["id"] == id:
                 return robot
-        return ""
+        abort(404)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
+    generateRobots()
     app.run(debug=True)
